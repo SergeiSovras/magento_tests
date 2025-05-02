bee9ee6ce932abb7072b0ddc877c3ba72c19ccc6
Sergei <sergei.sovras@gmail.com>
2025-05-02 16:11:09 +0300

    Headless tests

--- conftest.py
+++ conftest.py
@@ -3,11 +3,16 @@ from selenium import webdriver
 from pages.account_creating import AccountCreate
 from pages.eco_friendly import EcoFriendly
 from pages.sale import Sale
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture()
 def driver():
-    chrome_driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    chrome_driver = webdriver.Chrome(options=options)
     chrome_driver.maximize_window()
     yield chrome_driver
 
